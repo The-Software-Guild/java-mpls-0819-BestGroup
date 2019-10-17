8fc6e47e49259278b4a5a566175753b57a84bb87
David-Whitehouse <david.a.whitehouse90@gmail.com>
2019-10-17 13:05:33 -0500

    BestGroup Database Additions

--- BestGroupProject/StudyAbroadDb.sql
+++ BestGroupProject/StudyAbroadDb.sql
@@ -3,11 +3,19 @@ create database StudyAbroadDb;
 
 use StudyAbroadDb;
 
+
+
 create table `user`(
 `id` int primary key auto_increment,
 `username` varchar(30) not null unique,
 `password` varchar(100) not null,
-`enabled` boolean not null);
+`enabled` boolean not null,
+FirstName varchar(35) null,
+LastName varchar(35) null,
+SchoolName varchar (75) null,
+EmergencyContactName varchar (75) null,
+EmergencyContactPhone numeric (10) null
+);
 
 create table `role`(
 `id` int primary key auto_increment,
@@ -21,12 +29,45 @@ primary key(`user_id`,`role_id`),
 foreign key (`user_id`) references `user`(`id`),
 foreign key (`role_id`) references `role`(`id`));
 
+create table Trip (
+id int primary key auto_increment,
+`name` varchar (75) not null,
+StartDate date not null,
+EndDat date not null);
+
+create table UserTrip (
+TripId int not null,
+UserId int not null,
+primary key (TripId, UserId),
+foreign key (TripId) references Trip(id),
+foreign key (UserId) references `User`(id)
+);
+
+create table Category (
+id int primary key,
+`name` varchar (30) not null);
+
+create table `Event` (
+id int primary key auto_increment,
+`name` varchar (30) not null,
+StartTime datetime not null,
+EndTime datetime not null,
+Location varchar (100) null,
+Description varchar (500) not null,
+CategoryId int null,
+TransportId varchar(30) null,
+TripId int not null,
+foreign key (TripId) references Trip(id),
+foreign key (CategoryId) references Category(id));
+
+
+
 insert into `user`(`id`,`username`,`password`,`enabled`)
     values(1,"admin", "$2a$10$MYKoD5hXMPWABFYbgDkBy.HEwnwpcYzqSeog/ofBba7ahRkfFoGOi", true),
         (2,"user","$2a$10$MYKoD5hXMPWABFYbgDkBy.HEwnwpcYzqSeog/ofBba7ahRkfFoGOi",true);
 
 insert into `role`(`id`,`role`)
-    values(1,"ROLE_ADMIN"), (2,"ROLE_USER");
+    values(1,"Teacher"), (2,"Student");
     
 insert into `user_role`(`user_id`,`role_id`)
     values(1,1),(1,2),(2,2);
